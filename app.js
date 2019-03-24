a83c682911d5662de17372847cd4697ee89b93fc
sadesarrollo <48605986+sadesarrollo@users.noreply.github.com>
2019-03-24 14:21:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
-const listaUsuarios = document.getElementById('#lista-usuarios');
+const listaUsuarios = document.getElementById('lista-usuarios');
 
 // crear elemento de la lista & renderizarlo
-function renderUsers(doc){
-    let tr = document.createElement('tr');
-    let id = document.createElement('td');
-    let clientid = document.createElement('td');
-    let userid = document.createElement('td');
-    let url = document.createElement('td');
-    let fecha = document.createElement('td');    
+function renderUsers(doc){ 
 
-    tr.setAttribute('data-id', doc.id);
-    id.textContent = doc.data().id;
-    clientid.textContent = doc.data().ClientID;
-    userid.textContent = doc.data().UserID;
-    url.textContent = doc.data().Query;
-    fecha.textContent = doc.data().Fecha;        
-
-    tr.appendChild(tr);
-    tr.appendChild(id);
-    tr.appendChild(clientid);
-    tr.appendChild(userid);
-
-    var nuevaFila   = listaUsuarios.insertRow(listaUsuarios.rows.length);
-    var nuevaCelda  = nuevaFila.insertCell(0);
-    var nuevoTexto  = document.createTextNode('Texto');
-    nuevaCelda.appendChild(nuevoTexto);
-
-
+    var nuevaFila   = listaUsuarios.insertRow(1);
+    var nuevaCeldaID  = nuevaFila.insertCell(0);
+    var nuevaCeldaCID  = nuevaFila.insertCell(1);
+    var nuevaCeldaUID  = nuevaFila.insertCell(2);
+    var nuevaCeldaURL  = nuevaFila.insertCell(3);
+    var nuevaCeldaDATE  = nuevaFila.insertCell(4);
+    
+    nuevaCeldaID.innerHTML = doc.data().id;
+    nuevaCeldaCID.innerHTML = doc.data().ClientID;
+    nuevaCeldaUID.innerHTML = doc.data().UserID;
+    nuevaCeldaURL.innerHTML = doc.data().Query;
+    nuevaCeldaDATE.innerHTML = doc.data().Fecha;
 
 }
 
